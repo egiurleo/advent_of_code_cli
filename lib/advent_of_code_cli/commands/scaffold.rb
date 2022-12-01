b12a06422c982f3fbac9c46ecbbff3fb628f8816
Emily Samp <emily.samp@icloud.com>
2022-11-30 21:21:37 -0600

    Have scaffold write a template for the ruby code

--- lib/advent_of_code_cli/commands/scaffold.rb
+++ lib/advent_of_code_cli/commands/scaffold.rb
@@ -4,7 +4,21 @@ module AdventOfCodeCli
   module Commands
     class Scaffold < Command
       def execute
-        File.open("#{day_string}.rb", "w").close
+        File.open("#{day_string}.rb", "w") do |file|
+          file.puts <<~RUBY
+            module Day#{day_string}
+              class << self
+                def part_one(input)
+                  raise NotImplementedError
+                end
+
+                def part_two(input)
+                  raise NotImplementedError
+                end
+              end
+            end
+          RUBY
+        end
 
         Dir.mkdir("inputs") unless Dir.exist?("inputs")
         File.open("inputs/#{day_string}.txt", "w").close
